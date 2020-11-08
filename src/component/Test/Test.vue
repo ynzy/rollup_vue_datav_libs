a9137a3095cee3697d72101f29240fa28cd354d2
张勇 <758922096@qq.com>
2020-11-08 20:39:13 +0800

    feat(model:all): svg进阶

--- src/component/Test/Test.vue
+++ src/component/Test/Test.vue
@@ -1,17 +1,21 @@
 <!--  -->
 <template>
-<div>hello worasdld sdf1</div>
+<div>
+  <svg width="500" height="200" viewBox="0 0 50 20" style="border: 1px solid #000000">
+    <rect x="20" y="10" width="10" height=" 5" style="stroke: #000000; fill: none" />
+  </svg>
+  <svg width="500" height="200" style="border: 1px solid #000000">
+    <rect x="200" y="100" width="100" height="50" stroke-width="10" style="stroke: #000000; fill: none" />
+  </svg>
+  <svg preserveAspectRatio="xMidYMid meet" width="500" height="200" viewBox="0 0 200 200" style="border: 1px solid #000000" preserveAspectRatio="xMidYMid meet">
+    <rect x="100" y="100" width="100" height="50" stroke-width="10" style="stroke: #000000; fill: none" />
+  </svg>
+</div>
 </template>
 
 <script>
 export default {
   name: 'Test',
-  data() {
-    return {}
-  },
-  methods: {},
-  mounted() {},
-  components: {},
 }
 </script>
 
