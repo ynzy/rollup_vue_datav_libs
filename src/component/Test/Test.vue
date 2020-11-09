360f317d5a539f35ba6f6ae90993810fbec11b84
zhangyong <758922096@qq.com>
2020-11-09 11:26:35 +0800

    feat(model:all): svg组件

--- src/component/Test/Test.vue
+++ src/component/Test/Test.vue
@@ -1,16 +1,25 @@
 <!--  -->
 <template>
-<div>
-  <svg width="500" height="200" viewBox="0 0 50 20" style="border: 1px solid #000000">
-    <rect x="20" y="10" width="10" height=" 5" style="stroke: #000000; fill: none" />
-  </svg>
-  <svg width="500" height="200" style="border: 1px solid #000000">
-    <rect x="200" y="100" width="100" height="50" stroke-width="10" style="stroke: #000000; fill: none" />
-  </svg>
-  <svg preserveAspectRatio="xMidYMid meet" width="500" height="200" viewBox="0 0 200 200" style="border: 1px solid #000000" preserveAspectRatio="xMidYMid meet">
-    <rect x="100" y="100" width="100" height="50" stroke-width="10" style="stroke: #000000; fill: none" />
-  </svg>
-</div>
+  <div>
+    <svg width="0" height="0" viewBox="0 0 100 100">
+      <defs>
+        <symbol id="more" viewBox="0 0 100 100">
+          <circle r="5" cx="10" cy="25" fill="currentColor" />
+          <circle r="5" cx="10" cy="50" fill="currentColor" />
+          <circle r="5" cx="10" cy="75" fill="currentColor" />
+          <line x1="40" y1="25" x2="90" y2="25" stroke-width="8" stroke="currentColor" />
+          <line x1="40" y1="50" x2="90" y2="50" stroke-width="8" stroke="currentColor" />
+          <line x1="40" y1="75" x2="90" y2="75" stroke-width="8" stroke="currentColor" />
+        </symbol>
+      </defs>
+      <symbol id="filledArrowRight" viewBox="0 0 100 100">
+        <polyline points="20 10, 80 50, 20 90 " fill="currentColor" />
+      </symbol>
+      <symbol id="arrowRight" viewBox="0 0 100 100">
+        <polyline points="30 20, 70 50, 30 80 " fill="transparent" stroke="currentColor" stroke-width="3" />
+      </symbol>
+    </svg>
+  </div>
 </template>
 
 <script>
